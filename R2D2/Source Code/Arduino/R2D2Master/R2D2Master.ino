9508413c00cbefcfcbd21dc16c9dcdf51ff5d971
Frank Trocchia <ftrocchi@yahoo.com>
2015-07-17 11:28:34 -0400

    Added SendOnlySoftwareSerial

--- R2D2/Source Code/Arduino/R2D2Master/R2D2Master.ino
+++ R2D2/Source Code/Arduino/R2D2Master/R2D2Master.ino
@@ -1,5 +1,6 @@
 // R2D2Master.ino
 #include <SoftwareSerial.h>
+#include <SendOnlySoftwareSerial.h>
 
 #include "PS2.h"
 
@@ -7,6 +8,14 @@
 // PS2 DECLARATIONS
 //---------------------------------------------------------------------------------
 PS2 ps2 = PS2();
+
+//---------------------------------------------------------------------------------
+// WAVTRIGGER DECLARATIONS
+//---------------------------------------------------------------------------------
+#define WAV_PIN 6
+SendOnlySoftwareSerial wavSerial(WAV_PIN);
+byte masterVolume;
+bool enteringWavCode;
 
 //---------------------------------------------------------------------------------
 // SETUP AND LOOP
@@ -17,11 +26,19 @@ void setup() {
 
     // setup the PS2 controller, 9600 baud, pin 8 receive, pin 9 transmit
     ps2.init(9600, 8, 9);
+
+    // setup the motors
+
+    // setup the wav trigger
+    wavTriggerSetup();
 }
 
 void loop() {
     // Process the PS2 controller
     processPS2();
+
+    // Process the audio
+    processWavTrigger();
 }
 
 //---------------------------------------------------------------------------------
@@ -30,4 +47,31 @@ void loop() {
 void processPS2() {
     ps2.update();
 }
+
+//---------------------------------------------------------------------------------
+// WAVTRIGGER METHODS
+//---------------------------------------------------------------------------------
+void wavTriggerSetup() {
+    // wav trigger runs at 57600
+    wavSerial.begin(57600);
+
+    // wait for 2 seconds
+    delay(2000);
+
+    // turn off the onboard amp
+    //wavTrigger.ampPower(false);
+
+    // set the master volume
+    masterVolume = 150;
+    //wavTrigger.setMasterVolume(masterVolume);
+
+    // set the flag for entering the wav code from the controller
+    enteringWavCode = false;
+}
+
+void processWavTrigger() {
+    // TODO
+    
+}
+
 
